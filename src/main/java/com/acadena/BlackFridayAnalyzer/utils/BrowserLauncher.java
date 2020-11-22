3933393b41f1ac23a05809559ce1fa6c2f344c19
Albert Cadena <albert.cad.rub@gmail.com>
2020-11-22 16:52:52 +0100

    Added auto browser launcher for the H2 database UI.

---
+++ src/main/java/com/acadena/BlackFridayAnalyzer/utils/BrowserLauncher.java
@@ -0,0 +1,38 @@
+package com.acadena.BlackFridayAnalyzer.utils;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Component
+public class BrowserLauncher {
+    @EventListener({ApplicationReadyEvent.class})
+    void applicationReadyEvent() {
+        System.out.println("Application started ... launching browser now");
+        browse("http://localhost:8080/h2");
+    }
+
+    public static void browse(String url) {
+        if(Desktop.isDesktopSupported()){
+            System.setProperty("java.awt.headless", "false");
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
